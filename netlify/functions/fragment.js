50183b72113bdd8cd7f5c72b82256e7a69d7b8dc
Owen Buckley <owenbuckley13@gmail.com>
2023-05-15 21:45:54 -0400

    capture TODOs

--- netlify/functions/fragment.js
+++ netlify/functions/fragment.js
@@ -5,6 +5,7 @@ export async function handler (event, context) {
   const request = new Request(rawUrl, { headers });
   const response = await fragment(request);
 
+  // TODO need to handle all Response properties like headers
   return {
     statusCode: response.status,
     body: await response.text()
