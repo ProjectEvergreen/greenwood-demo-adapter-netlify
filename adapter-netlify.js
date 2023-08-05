75279c45c8e49c24249bb6297b2805b432c5b931
Owen Buckley <owenbuckley13@gmail.com>
2023-08-05 14:58:46 -0400

    sync with latest upstream adapter implementation

--- adapter-netlify.js
+++ adapter-netlify.js
@@ -1,30 +1,31 @@
-// https://docs.netlify.com/functions/deploy/?fn-language=js#custom-build-2
 import fs from 'fs/promises';
+import path from 'path';
 import { checkResourceExists } from '@greenwood/cli/src/lib/resource-utils.js';
 import { zip } from 'zip-a-folder';
 
+// https://docs.netlify.com/functions/create/?fn-language=js
 function generateOutputFormat(id) {
-  // TODO use `new Headers` here?
   return `
     import { handler as ${id} } from './__${id}.js';
 
     export async function handler (event, context) {
-      console.log('enter api handler for ${id}!');
-      const { rawUrl, headers } = event;
-      const request = new Request(rawUrl, { headers });
+      const { rawUrl, headers, httpMethod } = event;
+      const request = new Request(rawUrl, {
+        method: httpMethod,
+        headers: new Headers(headers)
+      });
       const response = await ${id}(request);
 
-      // TODO need to handle all Response properties like headers
       return {
         statusCode: response.status,
-        body: await response.text()
+        body: await response.text(),
+        headers: response.headers || new Headers()
       };
     }
-  `
+  `;
 }
 
 async function netlifyAdapter(compilation) {
-  console.log('ENTER netlifyAdapter');
   const { outputDir, projectDirectory, scratchDir } = compilation.context;
   const adapterOutputUrl = new URL('./netlify/functions/', scratchDir);
   const ssrPages = compilation.graph.filter(page => page.isSSR);
@@ -33,10 +34,6 @@ async function netlifyAdapter(compilation) {
   if (!await checkResourceExists(adapterOutputUrl)) {
     await fs.mkdir(adapterOutputUrl, { recursive: true });
   }
-
-  console.log({ ssrPages, apiRoutes, adapterOutputUrl });
-  console.log('compilation.context.outputDir ????', outputDir);
-  console.log('CWD (import.meta.url)????', import.meta.url);
 
   const files = await fs.readdir(outputDir);
   const isExecuteRouteModule = files.find(file => file.startsWith('execute-route-module'));
@@ -49,8 +46,7 @@ async function netlifyAdapter(compilation) {
 
     await fs.mkdir(outputRoot, { recursive: true });
     await fs.writeFile(new URL(`./${id}.js`, outputRoot), outputFormat);
-    // ~~TODO needed?~~ - yes!
-    await fs.writeFile(new URL(`./package.json`, outputRoot), JSON.stringify({
+    await fs.writeFile(new URL('./package.json', outputRoot), JSON.stringify({
       type: 'module'
     }));
 
@@ -72,8 +68,21 @@ async function netlifyAdapter(compilation) {
     if (isExecuteRouteModule) {
       await fs.cp(
         new URL(`./${isExecuteRouteModule}`, outputDir),
-        new URL(`./${isExecuteRouteModule}`, outputRoot),
-      )
+        new URL(`./${isExecuteRouteModule}`, outputRoot)
+      );
+    }
+
+    // TODO how to track SSR resources that get dumped out in the public directory?
+    // https://github.com/ProjectEvergreen/greenwood/issues/1118
+    const ssrPageAssets = (await fs.readdir(outputDir))
+      .filter(file => !path.basename(file).startsWith('_') && !path.basename(file).startsWith('execute') && path.basename(file).endsWith('.js'));
+
+    for (const asset of ssrPageAssets) {
+      await fs.cp(
+        new URL(`./${asset}`, outputDir),
+        new URL(`./${asset}`, outputRoot),
+        { recursive: true }
+      );
     }
 
     // TODO manifest options, like node version?
@@ -92,7 +101,7 @@ async function netlifyAdapter(compilation) {
 
     await fs.mkdir(outputRoot, { recursive: true });
     await fs.writeFile(new URL(`./api-${id}.js`, outputRoot), outputFormat);
-    await fs.writeFile(new URL(`./package.json`, outputRoot), JSON.stringify({
+    await fs.writeFile(new URL('./package.json', outputRoot), JSON.stringify({
       type: 'module'
     }));
 
@@ -103,11 +112,14 @@ async function netlifyAdapter(compilation) {
       new URL(`./__${id}.js`, outputRoot),
       { recursive: true }
     );
-    await fs.cp(
-      new URL(`./api/assets/`, outputDir),
-      new URL('./assets/', outputRoot),
-      { recursive: true }
-    );
+
+    if (await checkResourceExists(new URL('./api/assets/', outputDir))) {
+      await fs.cp(
+        new URL('./api/assets/', outputDir),
+        new URL('./assets/', outputRoot),
+        { recursive: true }
+      );
+    }
 
     // NOTE: All functions must live at the top level
     // # https://github.com/netlify/netlify-lambda/issues/90#issuecomment-486047201
