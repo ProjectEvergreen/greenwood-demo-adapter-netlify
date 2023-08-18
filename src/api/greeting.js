726010937bd66bc6791820bc09fedde7754f0ce1
Owen Buckley <owenbuckley13@gmail.com>
2023-08-18 10:02:41 -0400

    misc clean up

--- src/api/greeting.js
+++ src/api/greeting.js
@@ -4,11 +4,10 @@ export async function handler(request) {
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const name = params.has('name') ? params.get('name') : 'Greenwood';
   const body = { message: getMessage(name) };
-  const headers = new Headers();
-
-  headers.append('Content-Type', 'application/json');
 
   return new Response(JSON.stringify(body), {
-    headers
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
   });
 }
