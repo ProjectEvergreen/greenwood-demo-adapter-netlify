84aeb18daf99ca786cb402ce6242fb5f3db71536
Owen Buckley <owenbuckley13@gmail.com>
2025-03-20 11:42:24 -0400

    no nested API

---
+++ src/pages/api/event.js
@@ -0,0 +1,12 @@
+export async function handler(request) {
+  console.log({ request });
+  const data = await request.json();
+  const now = new Date().getTime();
+  const message = `Webhook "${data.name}" sent at "${data.time}" was successfully processed at "${now}".  Thank you!`;
+
+  return new Response(JSON.stringify({ message }), { 
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+}
