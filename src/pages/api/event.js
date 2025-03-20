414ea18fc00b69ead0a05c5a450ab18401c4f492
Owen Buckley <owenbuckley13@gmail.com>
2025-03-20 13:36:37 -0400

    misc

--- src/pages/api/event.js
+++ src/pages/api/event.js
@@ -1,5 +1,4 @@
 export async function handler(request) {
-  console.log({ request });
   const data = await request.json();
   const now = new Date().getTime();
   const message = `Webhook "${data.name}" sent at "${data.time}" was successfully processed at "${now}".  Thank you!`;
